7556fa81888eea6294ae54ede5c9b85c0772a1c7
perltech <noah.perlmutter@gmail.com>
2017-09-16 15:52:47 -0600

    hypno functionality

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,6 +1,5 @@
-
-
-$('#btn-start').on('click', function(){
+$(document).ready(function() {
+	$('#btn-start').on('click', function(){
 	//Gif generation goes here
 	var queryURL = 'https://api.giphy.com/v1/gifs/random?tag=futurama&api_key=dc6zaTOxFJmzC&limit=1';
 
@@ -14,4 +13,38 @@ $('#btn-start').on('click', function(){
 			$("#gif").html(img);
 			// $("#gif").append(div);
 		});
+});
+
+$('#hypno').on('click', function(){
+	//Gif generation goes here
+	var queryURL = 'https://api.giphy.com/v1/gifs/random?tag=futurama&api_key=dc6zaTOxFJmzC&limit=1';
+	var hypnoToad = 0;
+
+	sessionStorage.setItem('hypnoToad', hypnoToad);
+	$('#hypno-gif').empty();
+	hypnoToad++;		
+
+		$.get(queryURL)
+			.done(function(response){
+				
+				for (i=0; i < 250; i++) {
+					var img = $('<img>');
+					//var div = $('<div>');
+					var results = response.data;
+				
+					img.attr('src', results.image_original_url);
+					img.addClass("sm-gif");
+					//div.append(img);
+					$("#hypno-gif").append(img);
+				}
+			});
+
+	
+	if(hypnoToad == 5) {
+		$('#hypno-gif').empty();
+		queryURL = 'https://api.giphy.com/v1/gifs/random?tag=hypno+toad&api_key=dc6zaTOxFJmzC&limit=1'
+	}
+
+});
+
 });
